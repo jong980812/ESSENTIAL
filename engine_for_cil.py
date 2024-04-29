54afd32051cebe16577d7466ae6e0595d1550477
jong980812 <jong980812@khu.ac.kr>
2024-04-30 05:13:21 +0900

    Much mode

--- engine_for_cil.py
+++ engine_for_cil.py
@@ -191,8 +191,8 @@ def train_and_evaluate(model: torch.nn.Module, model_without_ddp: torch.nn.Modul
             model.module.transformer.eval()
             model.module.conv1.eval()
             for epoch in range(args.rehearsal_epochs):
-                # if (args.data_set=='SSV2') and (task_id==0):
-                #     break 
+                if (args.data_set=='SSV2') and (task_id==0):
+                    break 
                 if args.distributed:
                     data_loader[task_id]['rehearsal'].sampler.set_epoch(epoch) 
                 header = f'Task {task_id+1}/{args.num_tasks}  Rehearsal Epoch: [{epoch} / {args.rehearsal_epochs}]'
